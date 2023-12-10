3283b6f280adb41483a4f57ea8e72161d3362960
shiys <shiys1121@foxmail.com>
2023-12-10 22:37:36 +0800

    网页标题修改

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
-  title: '石云升 | 程序员',
+  title: '石云升 | 产品经理',
   description:
-    '我叫石云升，一名开发者，产品经理，同时也是一名创业多年的连续创业者。',
+    '我叫石云升，一名产品经理，同时也是一名创业多年的连续创业者。',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://yunyoute.cn'
