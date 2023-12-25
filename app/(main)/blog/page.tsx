2ef78a8f6429a05d2d5570005c9e94b8b6e2425d
286307088@qq.com <hehe@hehedeMac-mini.local>
2023-12-25 16:42:12 +0800

    隐藏rss订阅

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -1,6 +1,6 @@
 import Balancer from 'react-wrap-balancer'
 
-import { SocialLink } from '~/components/links/SocialLink'
+// import { SocialLink } from '~/components/links/SocialLink'
 import { Container } from '~/components/ui/Container'
 
 import { BlogPosts } from './BlogPosts'
@@ -32,9 +32,9 @@ export default function BlogPage() {
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
         </p>
-        <p className="flex items-center">
+        {/* <p className="flex items-center">
           <SocialLink href="/feed.xml" platform="rss" />
-        </p>
+        </p> */}
       </header>
       <div className="mt-12 grid grid-cols-1 gap-6 sm:mt-20 lg:grid-cols-2 lg:gap-8">
         <BlogPosts limit={20} />
